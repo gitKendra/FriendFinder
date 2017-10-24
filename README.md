daedf51e5f0f38b697752bd3899ab39bcdd07bab
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-10-24 09:35:54 -0400

    update readme and footer of htmls

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # FriendFinder
+
+The app matches a user with current friends stored in friends.js based on the total difference of their answers. After submitting the survey, a modal appears with your best match and you are added to the existing friends file for future matches.
+
+App that implements Node.js and Express utilizing front-end and back-end processes.
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -6,23 +6,22 @@ router.get('/api/friends', function(req, res) {
 	res.json(friends);
 });
 
-
-// Returns a JSON of the friend
+// Returns a JSON of the matched friend and adds the user to existing database
 router.post('/api/friends', function(req, res){
 	var user = req.body;
 	var matchIndex = 0;
 	var matchDifference = 0;
 
-	// i tracks the index of the friend from the array. j tracks the score
+	// i tracks the index of the friend from the array. j tracks the index from score array
 	for (var i = 0; i < friends.length; i++){
-		// Reset total difference for each friend comparison 
+		// Reset total difference at start of each friend comparison 
 		var totalDifference = 0;
 
 		// Calculate total difference beween user and friend
 		 for(var j = 0; j < 10; j ++){
 			totalDifference += Math.abs(friends[i].scores[j] - user.scores[j]);
 		}
-		// Compare and update match variables
+		// Compare current friend with current match and update variables
 		if(totalDifference < matchDifference){
 			matchDifference = totalDifference;
 			matchIndex = i;
@@ -33,10 +32,10 @@ router.post('/api/friends', function(req, res){
 		}
 	}
 
-	// add the new friend to exising friend
+	// Add the new friend to exising friend
 	friends.push(user);
 
-	// respond with the matched friend to the caller
+	// Respond with the matched friend
 	res.json(friends[matchIndex]);
 });
 
