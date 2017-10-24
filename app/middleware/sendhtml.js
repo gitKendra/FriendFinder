8b79b2788a8a5629f06168cb274e44306bea0eba
gitKendra <kendra.krzywicki@knights.ucf.edu>
2017-10-24 10:34:08 -0400

    use path for server-side location of files

--- app/middleware/sendhtml.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = function addSendHtml(req, res, next){
-	res.sendHtml = function(filepath){
-		return res.sendFile(filepath, {root:'../'});
-	};
-	next(req, res);
-};
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,16 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var path = require('path');
 
 router.get('/', function(req, res) {
-	res.sendFile('FriendFinder/app/public/home.html', {'root': '../'});
+	res.sendFile('home.html', { root: path.join(__dirname, '../public')});
 });
 
 router.get('/survey', function(req, res){
-	res.sendFile('FriendFinder/app/public/survey.html', {'root': '../'});
+	res.sendFile('survey.html', { root: path.join(__dirname, '../public')});
 });
 
 // Catch all route
 router.get('/:home', function(req, res) {
-	res.sendFile('FriendFinder/app/public/home.html', {'root': '../'});
+	res.sendFile('home.html', { root: path.join(__dirname, '../public')});
 });
 module.exports = router;
